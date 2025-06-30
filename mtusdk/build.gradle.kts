d858099c3ad526293cae5515eb8337197cf68942
meghana <meghana.signity@gettrx.com>
2025-06-30 15:55:13 +0530

    commit

---
+++ mtusdk/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.magtek.mtusdk"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(files("libs/mtusdk.aar"))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+group = "com.github.meghagettrx"
+version = "1.0.0"
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.github.meghagettrx"
+            artifactId = "mtusdk"
+            version = "1.0.0"
+
+            artifact(file("libs/mtusdk.aar")) {
+                extension = "aar"
+            }
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,14 +5,19 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        // Required for JitPack-style dependencies
+        maven(url = "https://jitpack.io")
+        flatDir {
+            dirs("mtusdk/libs") // or your correct path to mtusdk.aar
+        }
     }
 }
 
-rootProject.name = "My Application"
-include(":app")
- 
+rootProject.name = "MagtekWrapper"
+include(":mtusdk")
